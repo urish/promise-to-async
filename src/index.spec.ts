33d8376e8ed0bbc66a48308199d2627b11206af1
Uri Shaked <uri@urishaked.com>
2018-01-09 11:56:23 +0200

    feat: implement insertion of async keyword

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,8 +1,38 @@
 import { transform } from './index';
 
 describe('async-to-promise', () => {
-    it('should preserve comments in the source code', () => {
-        expect(transform('/* awesome */ function foo() { }').trim())
-            .toBe('/* awesome */ function foo() { }');
+    describe('async keyword insertion', () => {
+        it('should insert the `async` keyword before function declarations', () => {
+            expect(transform('/* awesome */ function foo() { }').trim())
+                .toBe('/* awesome */ async function foo() { }');
+        });
+
+        it('should insert the `async` keyword before arrow functions', () => {
+            expect(transform('const foo = () => 0').trim())
+                .toBe('const foo = async () => 0');
+        });
+
+        it('should insert the `async` keyword before private class methods', () => {
+            const input = `
+                class Test {
+                    private foo(bar) {
+                        bar.then(console.log);
+                    }
+                }
+            `;
+            const output = `
+                class Test {
+                    private async foo(bar) {
+                        bar.then(console.log);
+                    }
+                }
+            `;
+            expect(transform(input)).toBe(output);
+        });
+
+        it('should not modify a function which is already async', () => {
+            expect(transform('/* awesome */ async function foo() { }').trim())
+                .toBe('/* awesome */ async function foo() { }');
+        });
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,44 @@
 import * as ts from 'typescript';
 
+class Replacement {
+    constructor(readonly start: number, readonly end: number, readonly text = '') {
+    }
+}
+
+function isAsync(node: ts.Node) {
+    if (!node.modifiers) {
+        return false;
+    }
+    return node.modifiers.some((modifier) => modifier.kind === ts.SyntaxKind.AsyncKeyword);
+}
+
+function asyncInsertionPos(node: ts.FunctionLikeDeclarationBase) {
+    if (node.modifiers && node.modifiers[0]) {
+        return node.modifiers[0].getEnd() + 1;
+    }
+    return node.getStart();
+}
+
+function visit(node: ts.Node, replacements: Replacement[]) {
+    if ((ts.isFunctionDeclaration(node) || ts.isMethodDeclaration(node) || ts.isArrowFunction(node)) &&
+        !isAsync(node)) {
+        const insertionPos = asyncInsertionPos(node);
+        replacements.push(new Replacement(insertionPos, insertionPos, 'async '));
+    }
+    node.forEachChild((child) => visit(child, replacements));
+}
+
+function applyReplacements(source: string, replacements: Replacement[]) {
+    replacements = replacements.sort((r1, r2) => r1.end - r2.end);
+    for (const replacement of replacements) {
+        source = source.slice(0, replacement.start) + replacement.text + source.slice(replacement.end);
+    }
+    return source;
+}
+
 export function transform(source: string) {
     const sourceFile = ts.createSourceFile('test.ts', source, ts.ScriptTarget.Latest, true);
-    const printer = ts.createPrinter({ removeComments: false });
-    return printer.printFile(sourceFile);
+    const replacements = [] as Replacement[];
+    visit(sourceFile, replacements);
+    return applyReplacements(source, replacements);
 }
